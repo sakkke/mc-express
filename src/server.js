1f024ca3e8c605be49c931910ee9af1517b0a9e1
sakkke <84666033+sakkke@users.noreply.github.com>
2024-03-14 10:32:04 +0900

    chore: switch to src layout

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const { createServer } = require('./index')
+const { client_id, client_secret, discord_guild_id, discord_token, oauth2_callback, oauth2_endpoint, port } = require('../oauth2-discord-proxy.config.json')
+const { dynmap_port } = require('../config.json')
+
+createServer({
+  client_id,
+  client_secret,
+  discord_guild_id,
+  discord_token,
+  dynmap_port,
+  oauth2_callback,
+  oauth2_endpoint,
+  port,
+})
