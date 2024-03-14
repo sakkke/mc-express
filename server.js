de927ba4bc23fc070d241edf6672d472cf5da803
sakkke <84666033+sakkke@users.noreply.github.com>
2024-03-14 10:20:46 +0900

    refactor: add createServer function

--- server.js
+++ server.js
@@ -4,38 +4,62 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 const { client_id, client_secret, discord_guild_id, discord_token, oauth2_callback, oauth2_endpoint, port } = require('./oauth2-discord-proxy.config.json')
 const { dynmap_port } = require('./config.json')
 
-const app = express()
-
 const https = require('https')
 const fs = require('fs')
 
-const server = https.createServer({
-  key: fs.readFileSync('./key.pem'),
-  cert: fs.readFileSync('./cert.pem'),
-}, app)
+function createServer(config) {
+  const {
+    client_id,
+    client_secret,
+    discord_guild_id,
+    discord_token,
+    dynmap_port,
+    oauth2_callback,
+    oauth2_endpoint,
+    port,
+  } = config
 
-app.use('/', createProxy({
-  bot: createBot(discord_token),
+  const app = express()
+
+  const server = https.createServer({
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem'),
+  }, app)
+
+  app.use('/', createProxy({
+    bot: createBot(discord_token),
+    client_id,
+    client_secret,
+    discord_guild_id,
+    oauth2_callback,
+    oauth2_endpoint,
+  }))
+
+  app.get('/', (req, res) => {
+    res.redirect('/maps/')
+  })
+
+  app.use('/maps/', createProxyMiddleware({
+    target: `http://localhost:${dynmap_port}`,
+    changeOrigin: true,
+    pathRewrite: {
+      '^/maps/': '',
+    },
+  }))
+
+  server.listen(port, () => {
+    console.log(`listening at https://localhost:${port}`)
+    console.log(`login: https://localhost:${port}/login`)
+  })
+}
+
+createServer({
   client_id,
   client_secret,
   discord_guild_id,
+  discord_token,
+  dynmap_port,
   oauth2_callback,
   oauth2_endpoint,
-}))
-
-app.get('/', (req, res) => {
-  res.redirect('/maps/')
-})
-
-app.use('/maps/', createProxyMiddleware({
-  target: `http://localhost:${dynmap_port}`,
-  changeOrigin: true,
-  pathRewrite: {
-    '^/maps/': '',
-  },
-}))
-
-server.listen(port, () => {
-  console.log(`listening at https://localhost:${port}`)
-  console.log(`login: https://localhost:${port}/login`)
+  port,
 })
